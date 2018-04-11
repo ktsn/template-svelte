022dcd971dfd68c2a3c15375fc6efad337701962
ktsn <ktsn55@gmail.com>
2018-04-11 13:23:03 +0900

    feat: add css block handling

--- bili.config.js
+++ bili.config.js
@@ -1,5 +1,9 @@
+const fs = require('fs')
 const pkg = require('./package.json')
 const svelte = require('rollup-plugin-svelte')
+const postcss = require('postcss')
+const customProperties = require('postcss-custom-properties')
+const styleVars = require('./style-vars.json')
 
 const banner = `/*!
  * ${pkg.name} v${pkg.version}
@@ -18,5 +22,30 @@ function capitalize(name) {
 module.exports = {
   moduleName: capitalize(pkg.name),
   banner,
-  plugin: [svelte()]
+  plugin: [
+    svelte({
+      css: handleCss
+    })
+  ]
+}
+
+function handleCss({ code, map }) {
+  postcss({
+    plugins: [
+      customProperties({
+        variables: styleVars,
+        appendVariables: true
+      })
+    ]
+  })
+    .process(code, {
+      from: false,
+      map: {
+        prev: map
+      }
+    })
+    .then(({ css, map }) => {
+      fs.writeFileSync(`dist/${pkg.name}.css`, css)
+      fs.writeFileSync(`dist/${pkg.name}.css.map`, map)
+    })
 }
